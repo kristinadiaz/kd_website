286e14e640f92d6b3ccf93d5bca25ffeabce44d7
Kristina Diaz <kristina.diaz84@gmail.com>
2022-03-05 15:19:43 -0700

    started work on home page & nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import SplashPage from './components/SplashPage'
+import Home from './components/Home'
 // route to path='/home'
 
 export default function App () {
@@ -8,6 +9,7 @@ export default function App () {
     <div>
       <Routes>
         <Route path='/' element={<SplashPage />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
     </div>
   )
